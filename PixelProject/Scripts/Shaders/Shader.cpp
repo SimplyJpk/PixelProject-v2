21114b90fc4bcdd71f9a949b5bf2b75977e9c577
James Kellie <james_kellie@live.com>
2023-02-05 20:55:08 +1000

    Shader & ShaderManager, improved from v1
    - Better naming
    - More explicit demands for ShaderManager to prevent user error slightly.
    - More constexpr use to try optoimise some of the actions the Shader manager may need to do often.
    - Constexpr for constant methods where the conversion from sugar enums to opengl enums is required

---
+++ PixelProject/Scripts/Shaders/Shader.cpp
@@ -0,0 +1,50 @@
+﻿#include "Shader.h"
+#include <unordered_map>
+#include <stdexcept>
+
+Shader::Shader(const GLuint program_id)
+    : _program(program_id), _name(DEFAULT_SHADER_NAME)
+{
+}
+
+Shader::Shader(const GLuint program_id, const std::string& name)
+    : _program(program_id)
+    , _name(name)
+{
+}
+
+Shader::~Shader()
+{
+    glDeleteProgram(_program);
+}
+
+void Shader::UseProgram() const
+{
+   glUseProgram(_program);
+}
+
+GLuint Shader::GetProgramID() const
+{
+   return _program;
+}
+
+GLuint Shader::GetUniformLocation(const std::string& uniform_name)
+{
+   // Check if in map already
+   const auto i = _uniform_locations.find(uniform_name);
+   if (i != _uniform_locations.end())
+   {
+      return i->second;
+   }
+
+   // Check if exists in shader
+   auto pos = glGetUniformLocation(_program, uniform_name.c_str());
+   if (pos != -1)
+   {
+      _uniform_locations.insert({ uniform_name, pos });
+      return pos;
+   }
+
+   // Complain that it doesn't exist
+   throw std::runtime_error("Program [" + std::to_string(_program) + " '" + _name + "']: '" + uniform_name + "' does not exist.");
+}
