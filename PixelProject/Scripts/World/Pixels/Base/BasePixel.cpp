74ce00b84e763309b1c427ce05a29add9459d33b
James Kellie <james_kellie@live.com>
2023-02-19 14:45:36 +1000

    BasePixel v2
    A more rigid version of the BasePixel from v1, removed some unused variables, changed certain veriables to use smaller types (uint8_t instead of short/int).
    
    Important Changes:
    - Avoiding vtable, all variables are set in constructor and will be accessible through BasePixel& to avoid a handful of the wasteful calls made in v1. `GetNewPixel()` being one which added a virtual call where it really wasn't needed.
    - constexpr constructors to improve allocation time (tiny saving here)
    - v1 = 200~ bytes, v2 = just under 100~

---
+++ PixelProject/Scripts/World/Pixels/Base/BasePixel.cpp
@@ -0,0 +1,49 @@
+﻿#include "BasePixel.h"
+
+const uint8_t& BasePixel::GetPixelUpdateOrder()
+{
+   if (_pixel_update_order_count != 1)
+   {
+      _chunk_order_counter++;
+      if (_chunk_order_counter >= _pixel_update_order_count)
+      {
+         _chunk_order_counter = 0;
+      }
+   }
+   return *_pixel_update_order[_chunk_order_counter];
+}
+
+void BasePixel::InsertPixelUpdateOrder(const uint8_t index, const std::vector<Chunk::WorldDir>& directions)
+{
+   if (index < Pixels::PIXEL_MAX_UPDATE_ORDER_COUNT)
+   {
+      for (uint8_t i = 0; i < Chunk::NUM_DIRECTIONS; i++)
+      {
+         if (i < directions.size())
+            _pixel_update_order[index][i] = static_cast<uint8_t>(directions[i]);
+         else
+            _pixel_update_order[index][i] = Chunk::NUM_DIRECTIONS;
+      }
+   }
+}
+
+void BasePixel::InsertPixelUpdateOrder(const std::vector<std::vector<Chunk::WorldDir>>& directions)
+{
+   for (auto i = 0; i < directions.size(); i++)
+   {
+      InsertPixelUpdateOrder(static_cast<uint8_t>(i), directions[i]);
+   }
+}
+
+void BasePixel::SetPixelName(const char* const name)
+{
+   if (strlen(name) > 0)
+   {
+      strcpy_s(pixel_name, Pixels::PIXEL_MAX_UPDATE_ORDER_COUNT, name);
+   }
+}
+
+Uint32 BasePixel::GetRandomColour()
+{
+   return type_colours[rng() % colour_count];
+}
---
+++ PixelProject/Scripts/World/Pixels/Base/BasePixel.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <SDL_stdinc.h>
+#include <vector>
+
+#include "XoshiroCpp.hpp"
+
+#include "World/WorldConstants.h"
+
+class BasePixel
+{
+public:
+   Pixels::PixelType pixel_type;
+   char pixel_name[Pixels::PIXEL_MAX_NAME_LENGTH];
+   
+   bool is_updateable;
+   // TODO : (James) Need to apply pixel value to all PixelTypes
+   Uint32 new_pixel_value;
+
+   // TODO : (James) Check if worse by making static
+   XoshiroCpp::Xoshiro128Plus rng;
+
+   // Pixel Settings
+   uint8_t pixel_index;
+   uint8_t colour_count;
+
+   Uint32 type_colours[Pixels::PIXEL_MAX_COLOUR_COUNT];
+
+   const uint8_t& GetPixelUpdateOrder();
+   Uint32 GetRandomColour();
+
+protected:
+   uint8_t _chunk_order_counter;
+   uint8_t _pixel_update_order_count;
+   uint8_t _pixel_update_order[Pixels::PIXEL_MAX_UPDATE_ORDER_COUNT][Chunk::NUM_DIRECTIONS];
+
+   void InsertPixelUpdateOrder(const uint8_t index, const std::vector<Chunk::WorldDir>& directions);
+   void InsertPixelUpdateOrder(const std::vector<std::vector<Chunk::WorldDir>>& directions);
+   void SetPixelName(const char* const name);
+   
+};
