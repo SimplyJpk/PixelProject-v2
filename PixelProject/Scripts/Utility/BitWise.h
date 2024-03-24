4d0ded55d2715d79fcb22d0a9e246b23a9bb2b79
James Kellie <james_kellie@live.com>
2024-03-24 19:29:09 +1000

    optimise: Use constexpr inline with noexcept

--- PixelProject/Scripts/Utility/BitWise.h
+++ PixelProject/Scripts/Utility/BitWise.h
@@ -1,23 +1,23 @@
 ﻿#pragma once
 
 namespace PixelProject::Utility {
-inline void SetBit(unsigned int& value, const unsigned short bit)
-{
-   value |= (1 << bit);
-}
+   constexpr inline void SetBit(unsigned int& value, const unsigned short bit) noexcept
+   {
+      value |= (1U << bit);
+   }
 
-inline void ClearBit(unsigned int& value, const unsigned short bit)
-{
-   value &= ~(1 << bit);
-}
+   constexpr inline void ClearBit(unsigned int& value, const unsigned short bit) noexcept
+   {
+      value &= ~(1U << bit);
+   }
 
-inline bool IsBitSet(const unsigned int value, const unsigned short bit)
-{
-   return (value & (1 << bit)) != 0;
-}
+   constexpr inline bool IsBitSet(const unsigned int value, const unsigned short bit) noexcept
+   {
+      return (value & (1U << bit)) != 0;
+   }
 
-inline void ToggleBit(unsigned int& value, const unsigned short bit)
-{
-   value ^= (1 << bit);
-}
+   constexpr inline void ToggleBit(unsigned int& value, const unsigned short bit) noexcept
+   {
+      value ^= (1U << bit);
+   }
 }
